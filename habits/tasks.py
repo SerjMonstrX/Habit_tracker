8f67ffabdde5e6276dbe895be870fd53f3b8e950
SerjMonstrX <SerjMonstrX@gmail.com>
2024-06-25 00:05:25 +0700

    5. Настроены Сelery и функция отправки в ТГ

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,10 +1,10 @@
 import pytz
-import requests
+
 from celery import shared_task
 from datetime import datetime, timedelta
 
-from config import settings
 from habits.models import Habit
+from habits.services import send_tg_message
 
 
 @shared_task
@@ -16,6 +16,7 @@ def send_telegram_message():
     habits = Habit.objects.all()
 
     for habit in habits:
+
         # Проверяем, нужно ли отправить напоминание
         if habit.last_reminded and habit.last_reminded.date() == time_now.date():
             continue
@@ -23,11 +24,9 @@ def send_telegram_message():
         if time_now.time() >= habit.time and (not habit.last_reminded or habit.last_reminded.date() != time_now.date()):
             user = habit.creator
             chat_id = user.chat_id
+            habit_name = habit.name
+
             if chat_id:
-                params = {
-                    'text': f"Настало время для выполнения привычки: {habit.name}",
-                    'chat_id': chat_id,
-                }
-                requests.get(f'https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage', params=params)
+                send_tg_message(chat_id, habit_name)
                 habit.last_reminded = time_now
                 habit.save()
